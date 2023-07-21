92d66016ef7c42e89f3566eb8a93d276bdf167d6
cleverclope <cleverarinda@gmail.com>
2023-07-21 05:02:54 +0300

    added web icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaceScreenComponent } from './face-screen/face-screen.component';
+import { ProductionLeadComponent } from './production-lead/production-lead.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { TechSupportComponent } from './tech-support/tech-support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceScreenComponent,
+    ProductionLeadComponent,
+    DevelopersComponent,
+    TechSupportComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/face-screen/face-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-screen',
+  templateUrl: './face-screen.component.html',
+  styleUrls: ['./face-screen.component.scss']
+})
+export class FaceScreenComponent {
+
+}
